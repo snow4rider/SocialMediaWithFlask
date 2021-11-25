bf8001f9ee318f2aaca7d1940cb07826ba803e79
Matthew <matthew.t.mckenney@gmail.com>
2021-11-25 18:29:19 +0000

    added bootstrap to the front end.

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from flask import (
-  Flask, render_template
-  )
+from flask import Flask, render_template
 from flask_navigation import Navigation
 #from werkzeug.security import check_password_hash, generate_password_hash
 
-app = Flask(__name__)  # Create an Instance
+app = Flask(__name__) 
 nav = Navigation(app)
 
 nav.Bar('top',[
   nav.Item('Home', 'index'),
-  nav.Item('contact-me', 'contact_me'),
+  nav.Item('contact', 'contact'),
   nav.Item('about-me', 'about_me'),
 ])
 
@@ -19,14 +17,13 @@ def index():
   return render_template('index.html')
  
 
-@app.route('/contact_me')
-def contact_me():
-  return render_template('contact_me.html')
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
 
-@app.route('/about_me')
+@app.route('/about-me')
 def about_me():
   return render_template('about_me.html')
 
 
-app.run(host='0.0.0.0', port=5000,
-        debug=True)  # Run the Application (in debug mode)
+app.run(host='0.0.0.0', port=5000, debug=True)  # Run the Application (in debug mode)
